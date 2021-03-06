acdf99e8b87d8d7ad708f726c074d34fb837f6b7
Jang <wodus554@naver.com>
2021-03-06 17:33:00 +0900

    Initial Commit

---
+++ Test Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Test Project
+//
+//  Created by 장재연 on 2021/03/06.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
