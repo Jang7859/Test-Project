371637dc000f61432a56986424b4bcf26ca89c80
Jang7859 <wodus554@naver.com>
2021-03-08 20:47:45 +0900

    commit3

---
+++ test2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test2
+//
+//  Created by 장재연 on 2021/03/08.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!!\n");
+    return 0;
+}
